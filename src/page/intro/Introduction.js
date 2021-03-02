4ee0dc1f98d53d5d54e104c7fc20d64cdf285a27
NguyenVanDo51 <nguyenvando510@gmail.com>
2021-03-02 22:36:22 +0700

    fix hash router

--- src/page/intro/Introduction.js
+++ src/page/intro/Introduction.js
@@ -1,11 +1,25 @@
 import React from 'react';
+// import { Link } from 'react-router';
+// import feature from '../../style/scss/imgs/feature.png';
 
 class Introduction extends React.Component {
-    
     render() {
         return (
-            <h1>Đây là trang giới thiệu</h1>
-        )
+            <>
+                <div className="intro">
+                    <header className="header">
+                        <h3 className="header_title">Ứng dụng todo</h3>
+                        <p className="header_content">Quản lý, phân loại công việc. Đem đến cho bạn trải nghiệm thú vị và tuyệt vời.</p>
+                    </header>
+                    <div className="start">
+                        <button>Bắt đầu</button>
+                    </div>
+                    <div className="img_app">
+                        {/* <img src="" /> */}
+                    </div>
+                </div>
+            </>
+        );
     }
 }
 
