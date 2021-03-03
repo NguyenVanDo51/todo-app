dce02d31ce21108203d72715f59fb9013f2d5651
NguyenVanDo51 <nguyenvando510@gmail.com>
2021-03-03 08:10:14 +0700

    add welcome page

--- src/page/intro/Introduction.js
+++ src/page/intro/Introduction.js
@@ -1,22 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 // import { Link } from 'react-router';
 // import feature from '../../style/scss/imgs/feature.png';
+import left_img from '../../style/scss/imgs/welcome-left.png';
+import right_img from '../../style/scss/imgs/welcome-right.png';
+import logo from '../../style/scss/imgs/logo_login.png';
 
 class Introduction extends React.Component {
     render() {
         return (
             <>
                 <div className="intro">
-                    <header className="header">
-                        <h3 className="header_title">Ứng dụng todo</h3>
-                        <p className="header_content">Quản lý, phân loại công việc. Đem đến cho bạn trải nghiệm thú vị và tuyệt vời.</p>
-                    </header>
-                    <div className="start">
-                        <button>Bắt đầu</button>
-                    </div>
-                    <div className="img_app">
-                        {/* <img src="" /> */}
-                    </div>
+                    <section className="container">
+                        <div className="row">
+                            {/* <div className="col-12 intro_container"> */}
+                            <div className="col-3">
+                                <img className="intro_img left" src={left_img} alt="left welcome" />
+                            </div>
+                            <div className="col-3">
+                                <div className="center">
+                                    <img src={logo} alt="logo" className="intro_logo mb-3" />
+                                    <h3 className="header_title mb-3">D. Todo List</h3>
+                                    <p className="header_content mb-3">Quản lý, phân loại công việc. Đem đến cho bạn trải nghiệm thú vị và tuyệt vời.</p>
+                                    <a role="button" href="/login" className="btn_start">
+                                        Bắt đầu
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="col-3">
+                                <img className="intro_img left" src={right_img} alt="left welcome" />
+                            </div>
+                            {/* </div> */}
+                        </div>
+                    </section>
                 </div>
             </>
         );
